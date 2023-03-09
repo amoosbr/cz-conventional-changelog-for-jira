2b8e5b649a53f064411ea99ca02d4657d9707290
amoosbr <amoosbr@users.noreply.github.com>
2023-03-09 08:45:37 +0100

    build: update config

--- index.js
+++ index.js
@@ -1,53 +1,4 @@
 const custom = require("@digitalroute/cz-conventional-changelog-for-jira/configurable");
-// You can do this optionally if you want to extend the commit types
-// const defaultTypes = require("@digitalroute/cz-conventional-changelog-for-jira/types");
-
-// "types": {
-//     "feat": {
-//       "description": "A new feature",
-//       "title": "Features"
-//     },
-//     "fix": {
-//       "description": "A bug fix",
-//       "title": "Bug Fixes"
-//     },
-//     "docs": {
-//       "description": "Documentation only changes",
-//       "title": "Documentation"
-//     },
-//     "style": {
-//       "description": "Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)",
-//       "title": "Styles"
-//     },
-//     "refactor": {
-//       "description": "A code change that neither fixes a bug nor adds a feature",
-//       "title": "Code Refactoring"
-//     },
-//     "perf": {
-//       "description": "A code change that improves performance",
-//       "title": "Performance Improvements"
-//     },
-//     "test": {
-//       "description": "Adding missing tests or correcting existing tests",
-//       "title": "Tests"
-//     },
-//     "build": {
-//       "description": "Changes that affect the build system or external dependencies (example scopes: gulp, broccoli, npm)",
-//       "title": "Builds"
-//     },
-//     "ci": {
-//       "description": "Changes to our CI configuration files and scripts (example scopes: Travis, Circle, BrowserStack, SauceLabs)",
-//       "title": "Continuous Integrations"
-//     },
-//     "chore": {
-//       "description": "Other changes that don't modify src or test files",
-//       "title": "Chores"
-//     },
-//     "revert": {
-//       "description": "Reverts a previous commit",
-//       "title": "Reverts"
-//     }
-//   }
 
 module.exports = custom({
   types: {
@@ -64,8 +15,7 @@ module.exports = custom({
       title: "Documentation",
     },
     refactor: {
-      description:
-        "A code change that neither fixes a bug nor adds a feature (formatting, performance improvement, etc)",
+      description: "A code change that neither fixes a bug nor adds a feature",
       title: "Code Refactoring",
     },
     test: {
@@ -74,7 +24,7 @@ module.exports = custom({
     },
     build: {
       description:
-        "Changes that affect the build system or external dependencies (npm, webpack, typescript)",
+        "Changes that affect the build system or external dependencies (npm, pip, ts, gradle)",
       title: "Builds",
     },
     ci: {
@@ -82,17 +32,23 @@ module.exports = custom({
         "Changes to our CI configuration files and scripts (NOTE: Does not bump the version)",
       title: "Continuous Integrations",
     },
-    // chore: {
-    //   description: "Other changes that don't modify src or test files",
-    //   title: 'Chores'
-    // },
+    style: {
+      description:
+        "Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)",
+      title: "Style",
+    },
+    perf: {
+      description: "A code change that improves performance",
+      title: "Performance",
+    },
     revert: {
       description: "Reverts a previous commit",
       title: "Reverts",
     },
-    perf: {
-      description: "Improvements that will make your code perform better",
-      title: "Performance",
+    chore: {
+      description: "Other changes that don't modify src or test files",
+      title: "Chores",
     },
   },
+  jiraLocation: "pre-type",
 });
